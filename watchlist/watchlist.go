4bff8c20e70d102db00e4b8319b09127f2671509
Luke Williams <luke@iquidus.io>
2019-05-16 12:40:55 +0200

    update topia addresses

--- watchlist/watchlist.go
+++ watchlist/watchlist.go
@@ -8,7 +8,9 @@ type Address struct {
 func GetWatchlist() ([]Address) {
   var addresses []Address
   //addresses = append(addresses, Address{Hash: "0xda904bc07fd95e39661941b3f6daded1b8a38c71", Label: "Test"})
-  addresses = append(addresses, Address{Hash: "0xde89c4687984d7cb91cacdd084003ffdf36e493a", Label: "Cryptopia - UBQ"})
-  addresses = append(addresses, Address{Hash: "0x6b7bcaebcbe0b92f879cfe5ed2cdb34247d49f0d", Label: "Cryptopia - ERC20"})
+  addresses = append(addresses, Address{Hash: "0xde89c4687984d7cb91cacdd084003ffdf36e493a", Label: "Cryptopia - UBQ - OLD"})
+  addresses = append(addresses, Address{Hash: "0x6b7bcaebcbe0b92f879cfe5ed2cdb34247d49f0d", Label: "Cryptopia - ERC20 - OLD"})
+  addresses = append(addresses, Address{Hash: "0xabee6c9855af9202f995efa7eea46c7819eafe09", Label: "Cryptopia - UBQ"})
+  addresses = append(addresses, Address{Hash: "0x81e8416fabcfb122964b61e24e8b005fb1c7081b", Label: "Cryptopia - ERC20"})
   return addresses
 }
